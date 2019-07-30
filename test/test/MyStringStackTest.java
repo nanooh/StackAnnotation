7db8f5c3a462b13729317a921f31edf948256e4b
nanooh <36320156+nanooh@users.noreply.github.com>
2019-07-30 13:45:38 +0800

    fixed error

--- test/test/MyStringStackTest.java
+++ test/test/MyStringStackTest.java
@@ -36,8 +36,8 @@ public class MyStringStackTest extends TestCase {
 		assertTrue (stack.isEmpty());
         stack.push (s1);
         stack.push (s2);
-        // assertFalse (stack.isEmpty());
-	assertTrue (stack.isEmpty());
+        assertFalse (stack.isEmpty());
+	// assertTrue (stack.isEmpty());
 	}
 
 	@Test
